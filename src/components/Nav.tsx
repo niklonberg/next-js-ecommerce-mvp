96a20cba543a9734b79fe7738592f87e72ab84d4
Nikolai <niklonberg@gmail.com>
2024-04-24 15:49:07 +0200

    fix using incorrect Link component, needed to be nextjs <Link>

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Link } from "lucide-react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ComponentProps, ReactNode } from "react";
 
