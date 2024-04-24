acc8a08795d863c3c775927969424daaf1ffc10b
Nikolai <niklonberg@gmail.com>
2024-04-24 16:27:12 +0200

    add starting AdminDashboard using shadcn-ui Card components

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,23 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function AdminDashboard() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sales</CardTitle>
+          <CardDescription>placeholder desc</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>This is the card content!</p>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
