526a0ae04c66426b562598c94e6b1e7d9795000b
Nikolai <niklonberg@gmail.com>
2024-04-25 14:07:24 +0200

    add new product link, boilerplate ProductsTable

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,11 +1,47 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function AdminProducts() {
   return (
     <div className="flex justify-between items-center gap-4">
       <PageHeader>Products</PageHeader>
-      <Button>Add Product</Button>
+      <Button asChild>
+        <Link href="/admin/products/new">Add Product</Link>
+      </Button>
     </div>
+  );
+}
+
+function ProductsTable() {
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">INV001</TableCell>
+          <TableCell>Paid</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   );
 }
