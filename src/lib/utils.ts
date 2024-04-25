605a5a9657674e8aa345645d935395c5f5123c32
Nikolai <niklonberg@gmail.com>
2024-04-25 12:13:16 +0200

    add waitTest util fn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function waitTest(waitDuration: number) {
+  return new Promise((resolve) => setTimeout(resolve, waitDuration));
 }
