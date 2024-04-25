d758e3627425cb544403cdc3e2f777eaea93f6f8
Nikolai <niklonberg@gmail.com>
2024-04-25 14:45:58 +0200

    edit ProductForm, add input/label for name & price, setup priceInCents state

--- src/app/admin/products/new/_components/ProductForm.tsx
+++ src/app/admin/products/new/_components/ProductForm.tsx
@@ -1,5 +1,29 @@
 "use client";
 
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export function ProductForm() {
-  return <div></div>;
+  const [priceInCents, setPriceInCents] = React.useState<number>(0);
+
+  return (
+    <form className="space-y-8">
+      <div className="space-y-2">
+        <Label htmlFor="name">Name</Label>
+        <Input type="text" id="name" name="name" required></Input>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="priceInCents">Price in cents</Label>
+        <Input
+          type="number"
+          id="priceInCents"
+          name="priceInCents"
+          required
+          value={priceInCents}
+          onChange={(e) => setPriceInCents(Number(e.target.value))}
+        ></Input>
+      </div>
+    </form>
+  );
 }
