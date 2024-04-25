7df20161702df52c512224ff4c67281f4087dd41
Nikolai <niklonberg@gmail.com>
2024-04-25 14:53:58 +0200

    add price in usd visual aid, adjust css spacing in form

--- src/app/admin/products/new/_components/ProductForm.tsx
+++ src/app/admin/products/new/_components/ProductForm.tsx
@@ -3,12 +3,13 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { formatCurrency } from "@/lib/formatters";
 
 export function ProductForm() {
-  const [priceInCents, setPriceInCents] = React.useState<number>(0);
+  const [priceInCents, setPriceInCents] = React.useState<number>();
 
   return (
-    <form className="space-y-8">
+    <form className="space-y-6">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
         <Input type="text" id="name" name="name" required></Input>
@@ -21,8 +22,11 @@ export function ProductForm() {
           name="priceInCents"
           required
           value={priceInCents}
-          onChange={(e) => setPriceInCents(Number(e.target.value))}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
         ></Input>
+      </div>
+      <div className="text-muted-foreground">
+        Price in USD: {formatCurrency((priceInCents || 0) / 100)}
       </div>
     </form>
   );
