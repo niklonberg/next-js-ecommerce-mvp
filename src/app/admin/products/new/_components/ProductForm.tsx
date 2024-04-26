82b2b412975cd365614cced7866dc565c222525c
Nikolai <niklonberg@gmail.com>
2024-04-26 12:40:03 +0200

    input fields for description, file, image and button for submit

--- src/app/admin/products/new/_components/ProductForm.tsx
+++ src/app/admin/products/new/_components/ProductForm.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { formatCurrency } from "@/lib/formatters";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 export function ProductForm() {
   const [priceInCents, setPriceInCents] = React.useState<number>();
@@ -12,7 +14,7 @@ export function ProductForm() {
     <form className="space-y-6">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required></Input>
+        <Input type="text" id="name" name="name" required />
       </div>
       <div className="space-y-2">
         <Label htmlFor="priceInCents">Price in cents</Label>
@@ -23,11 +25,36 @@ export function ProductForm() {
           required
           value={priceInCents}
           onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
-        ></Input>
+        />
       </div>
       <div className="text-muted-foreground">
         Price in USD: {formatCurrency((priceInCents || 0) / 100)}
       </div>
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Textarea id="description" name="description" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input
+          className="hover:cursor-pointer"
+          type="file"
+          id="file"
+          name="file"
+          required
+        />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="image">File</Label>
+        <Input
+          className="hover:cursor-pointer"
+          type="file"
+          id="image"
+          name="image"
+          required
+        />
+      </div>
+      <Button type="submit">Save</Button>
     </form>
   );
 }
