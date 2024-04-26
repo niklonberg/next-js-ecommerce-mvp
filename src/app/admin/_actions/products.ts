4a7029e787893352d40ac56ce2dc0e707b4b73b3
Nikolai <niklonberg@gmail.com>
2024-04-26 16:01:17 +0200

    add deleteProduct

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -3,7 +3,7 @@
 import db from "@/db/db";
 import { z } from "zod";
 import fs from "fs/promises";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 const fileSchema = z.instanceof(File, { message: "File type is required" });
 const imageSchema = fileSchema.refine(
@@ -57,4 +57,9 @@ export async function toggleProductPurchasable(
   purchasable: boolean
 ) {
   await db.product.update({ where: { id }, data: { purchasable } });
+}
+
+export async function deleteProduct(id: string) {
+  const product = db.product.delete({ where: { id } });
+  if (!product) return notFound();
 }
