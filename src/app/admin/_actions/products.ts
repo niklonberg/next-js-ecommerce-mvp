bb3c99227c16f8695f7b14bb62d5b632129997e8
Nikolai <niklonberg@gmail.com>
2024-04-26 12:59:34 +0200

    edit z schema for file & image inputs

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -2,10 +2,14 @@
 
 import { z } from "zod";
 
+const fileSchema = z.instanceof(File, { message: "File type is required" });
+
 z.object({
   name: z.string().min(1),
   priceInCents: z.coerce.number().int().min(1),
   description: z.string().min(1),
+  file: fileSchema.refine((file) => file.size > 0, "Filesize is too low"),
+  image: fileSchema.refine((file) => file.size > 0, "Filesize is too low"),
 });
 
 export async function addProduct(formData: FormData) {
