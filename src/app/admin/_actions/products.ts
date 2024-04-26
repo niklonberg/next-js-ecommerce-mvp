059303ecbc42d0ffcdb6ae4c17705d56cae326d8
Nikolai <niklonberg@gmail.com>
2024-04-26 12:56:49 +0200

    add zod schema, which functions as our server validation for the FormData ProductForm sends.

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,5 +1,13 @@
 "use server";
 
+import { z } from "zod";
+
+z.object({
+  name: z.string().min(1),
+  priceInCents: z.coerce.number().int().min(1),
+  description: z.string().min(1),
+});
+
 export async function addProduct(formData: FormData) {
   console.log(formData);
 }
