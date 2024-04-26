1dc7ca1bebfa8c540ef3c0773a2b340d9a4e15eb
Nikolai <niklonberg@gmail.com>
2024-04-26 13:03:08 +0200

    edit imageSchema to ensure the correct file type

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -3,13 +3,16 @@
 import { z } from "zod";
 
 const fileSchema = z.instanceof(File, { message: "File type is required" });
+const imageSchema = fileSchema.refine(
+  (file) => file.size === 0 || file.type.startsWith("image/")
+);
 
 z.object({
   name: z.string().min(1),
   priceInCents: z.coerce.number().int().min(1),
   description: z.string().min(1),
   file: fileSchema.refine((file) => file.size > 0, "Filesize is too low"),
-  image: fileSchema.refine((file) => file.size > 0, "Filesize is too low"),
+  image: imageSchema,
 });
 
 export async function addProduct(formData: FormData) {
