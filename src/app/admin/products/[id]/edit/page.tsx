74cb27c97bad8371e13aa63ef83aaabb9327fe6b
Nikolai <niklonberg@gmail.com>
2024-04-26 17:23:25 +0200

    add EditProduct page

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,18 @@
+import { PageHeader } from "@/app/admin/_components/PageHeader";
+import { ProductForm } from "../../_components/ProductForm";
+import db from "@/db/db";
+
+export default async function EditProduct({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await db.product.findUnique({ where: { id } });
+
+  return (
+    <>
+      <PageHeader>Add Product</PageHeader>
+      <ProductForm product={product} />
+    </>
+  );
+}
