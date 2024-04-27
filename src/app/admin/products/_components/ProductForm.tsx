4891bfb41096a176c2eb765fea6f767cc01607fe
Nikolai <niklonberg@gmail.com>
2024-04-27 09:47:38 +0200

    set ProductForm to use either addProduct or updateProduct based on the value of product passed to it

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -6,13 +6,16 @@ import { Label } from "@/components/ui/label";
 import { formatCurrency } from "@/lib/formatters";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { addProduct } from "@/app/admin/_actions/products";
+import { addProduct, updateProduct } from "@/app/admin/_actions/products";
 import { useFormStatus, useFormState } from "react-dom";
 import { Product } from "@prisma/client";
 import Image from "next/image";
 
 export function ProductForm({ product }: { product?: Product | null }) {
-  const [error, action] = useFormState(addProduct, {});
+  const [error, action] = useFormState(
+    product == null ? addProduct : updateProduct.bind(null, product.id),
+    {}
+  );
   const [priceInCents, setPriceInCents] = React.useState<number | undefined>(
     product?.priceInCents
   );
