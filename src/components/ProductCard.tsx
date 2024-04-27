0af0a681b971711c0502bef8cecd5671617a0637
Nikolai <niklonberg@gmail.com>
2024-04-27 14:29:41 +0200

    add ProductCard component, ProductCardProps type

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,48 @@
+import { formatCurrency } from "@/lib/formatters";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
+type ProductCardProps = {
+  id: string;
+  name: string;
+  priceInCents: number;
+  description: string;
+  imagePath: string;
+};
+
+export function ProductCard({
+  id,
+  name,
+  priceInCents,
+  description,
+  imagePath,
+}: ProductCardProps) {
+  return (
+    <Card>
+      <div>
+        <Image src={imagePath} alt={name} />
+      </div>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{description}</p>
+      </CardContent>
+      <CardFooter>
+        <Button asChild>
+          <Link href={`/products/${id}/purchase`}>Purchase</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
