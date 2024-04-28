fb77098019735644e441cad6a716101f17539b61
Nikolai <niklonberg@gmail.com>
2024-04-28 09:42:30 +0200

    edit PurchasePage so it gets product id from url, and finds it in the database

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,3 +1,13 @@
-export default function PurchasePage() {
+import db from "@/db/db";
+import { notFound } from "next/navigation";
+
+export default async function PurchasePage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await db.product.findUnique({ where: { id } });
+  if (!product) return notFound();
+
   return <h1>Hi penelope</h1>;
 }
