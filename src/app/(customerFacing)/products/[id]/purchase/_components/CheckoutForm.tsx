4eea4eb747ed3f1c9fecdef563e77c292f8470c4
Nikolai <niklonberg@gmail.com>
2024-04-28 10:51:43 +0200

    add CheckoutForm component

---
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -0,0 +1,8 @@
+type CheckoutFormProps = {
+  product: {};
+  clientSecret: string;
+};
+
+export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
+  return <p>form</p>;
+}
--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,6 +1,7 @@
 import db from "@/db/db";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
+import { CheckoutForm } from "./_components/CheckoutForm";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
