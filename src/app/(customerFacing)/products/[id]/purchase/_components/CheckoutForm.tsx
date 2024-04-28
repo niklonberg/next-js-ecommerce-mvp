d756332ff64e2408d0bd028a63c72c0b8856407a
Nikolai <niklonberg@gmail.com>
2024-04-28 11:12:40 +0200

    edit CheckoutForm to render relevant information, aswell as stripe Elements (the credit card related stuff)

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,8 +1,55 @@
+"use client";
+
+import { formatCurrency } from "@/lib/formatters";
+import {
+  Elements,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Image from "next/image";
+
 type CheckoutFormProps = {
-  product: {};
+  product: {
+    imagePath: string;
+    name: string;
+    priceInCents: number;
+    description: string;
+  };
   clientSecret: string;
 };
 
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
+);
+
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
-  return <p>form</p>;
+  return (
+    <div className="max-w-5xl w-full mx-auto space-y-6">
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image src={product.imagePath} fill alt={product.name} />
+        </div>
+        <div>
+          <div className="text-lg">
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <p className="line-clamp-4 text-muted-foreground">
+            {product.description}
+          </p>
+        </div>
+      </div>
+      <Elements options={{ clientSecret }} stripe={stripePromise}>
+        <Form />
+      </Elements>
+    </div>
+  );
+}
+
+function Form() {
+  const stripe = useStripe();
+  const elements = useElements();
+  return <PaymentElement />;
 }
