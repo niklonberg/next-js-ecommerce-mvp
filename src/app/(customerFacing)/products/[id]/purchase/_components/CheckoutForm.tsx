6b1487e7fcb574d03dc37020887fc3d8c6064bcd
Nikolai <niklonberg@gmail.com>
2024-04-29 09:29:45 +0200

    set isLoading state, add handleSubmit fn, tie submit button to state

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -18,6 +18,7 @@ import {
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import Image from "next/image";
+import { FormEvent, useState } from "react";
 
 type CheckoutFormProps = {
   product: {
@@ -67,8 +68,18 @@ export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
 function Form({ priceInCents }: { priceInCents: number }) {
   const stripe = useStripe();
   const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (!stripe || !elements) return;
+
+    setIsLoading(true);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Card>
         <CardHeader>
           <CardTitle>Checkout</CardTitle>
@@ -80,8 +91,14 @@ function Form({ priceInCents }: { priceInCents: number }) {
           <PaymentElement />
         </CardContent>
         <CardFooter>
-          <Button className="w-full" size="lg" disabled={!stripe || !elements}>
-            Purchase - {formatCurrency(priceInCents / 100)}
+          <Button
+            className="w-full"
+            size="lg"
+            disabled={!stripe || !elements || isLoading}
+          >
+            {isLoading
+              ? "Purchasing..."
+              : `Purchase - ${formatCurrency(priceInCents / 100)}`}
           </Button>
         </CardFooter>
       </Card>
