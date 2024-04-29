2f4a26a0528f953fe581337287663de5c3fab8e3
Nikolai <niklonberg@gmail.com>
2024-04-29 10:32:30 +0200

    edit CheckoutFormProps type to include product id, add check for if customer has already purchased a product, preventing accidental double purchases

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -23,6 +23,7 @@ import { FormEvent, useState } from "react";
 
 type CheckoutFormProps = {
   product: {
+    id: string;
     imagePath: string;
     name: string;
     priceInCents: number;
@@ -60,27 +61,43 @@ export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
         </div>
       </div>
       <Elements options={{ clientSecret }} stripe={stripePromise}>
-        <Form priceInCents={product.priceInCents} />
+        <Form priceInCents={product.priceInCents} productId={product.id} />
       </Elements>
     </div>
   );
 }
 
-function Form({ priceInCents }: { priceInCents: number }) {
+function Form({
+  priceInCents,
+  productId,
+}: {
+  priceInCents: number;
+  productId: string;
+}) {
   const stripe = useStripe();
   const elements = useElements();
   const [isLoading, setIsLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState<string>();
   const [email, setEmail] = useState<string>();
 
-  function handleSubmit(e: FormEvent) {
+  async function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
     if (!stripe || !elements || !email) return;
 
     setIsLoading(true);
 
-    // Check for existing order
+    const orderExists = await userOrderExists(email, productId);
+
+    if (orderExists) {
+      setErrorMsg(
+        `You have already purchased this Product. Go to ${(
+          <a>My Orders</a>
+        )} to download it`
+      );
+      setIsLoading(false);
+      return;
+    }
 
     stripe
       .confirmPayment({
