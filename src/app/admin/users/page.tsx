0c5814d32a4b2424a243d0d877bcf8576f18387e
Nikolai <niklonberg@gmail.com>
2024-04-30 10:19:14 +0200

    set up user table

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,5 +1,23 @@
 import db from "@/db/db";
 import { PageHeader } from "../_components/PageHeader";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency } from "@/lib/formatters";
+import { MoreVertical } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DeleteDropdownItem } from "./_components/UserActions";
 
 async function getUsers() {
   return await db.user.findMany({
@@ -12,8 +30,58 @@ async function getUsers() {
   });
 }
 
-async function UsersTable() {}
+async function UsersTable() {
+  const users = await getUsers();
+  if (users.length === 0) return <p>No customers found</p>;
+
+  return (
+    <Table>
+      <TableCaption>Current customers.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Id</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead>Value</TableHead>
+          <TableHead>
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((user) => {
+          const pricePaidInCents = user.orders.reduce(
+            (acc, curr) => acc + curr.pricePaidInCents,
+            0
+          );
+          return (
+            <TableRow key={user.id}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{formatCurrency(pricePaidInCents / 100)}</TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <MoreVertical />
+                    <span className="sr-only">Actions</span>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DeleteDropdownItem />
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+}
 
 export default function UsersPage() {
-  return <PageHeader>Customers</PageHeader>;
+  return (
+    <>
+      <PageHeader>Customers</PageHeader>
+      <UsersTable />
+    </>
+  );
 }
