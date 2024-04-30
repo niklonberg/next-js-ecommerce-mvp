e9d0f35b4cf65f1eb13f2af6d89e9310038c20d8
Nikolai <niklonberg@gmail.com>
2024-04-30 11:10:02 +0200

    fix use of async in DeleteDropdownItem, not supported for client components and not needed either

--- src/app/admin/users/_components/UserActions.tsx
+++ src/app/admin/users/_components/UserActions.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { deleteUser } from "../../_actions/users";
 
-export async function DeleteDropdownItem({ id }: { id: string }) {
+export function DeleteDropdownItem({ id }: { id: string }) {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
 
