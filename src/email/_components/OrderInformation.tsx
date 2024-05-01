33f449632c7fd8942a7893edb564f6e990a96d3c
Nikolai <niklonberg@gmail.com>
2024-05-01 11:28:33 +0200

    add product section

--- src/email/_components/OrderInformation.tsx
+++ src/email/_components/OrderInformation.tsx
@@ -1,9 +1,16 @@
 import { formatCurrency } from "@/lib/formatters";
-import { Column, Row, Section, Text } from "@react-email/components";
+import {
+  Button,
+  Column,
+  Img,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
 
 type OrderInformationProps = {
   order: { id: string; createdAt: Date; pricePaidInCents: number };
-  product: {};
+  product: { imagePath: string; name: string; description: string };
   downloadVerificationId: string;
 };
 
@@ -15,31 +22,60 @@ export function OrderInformation({
   downloadVerificationId,
 }: OrderInformationProps) {
   return (
-    <Section>
-      <Row>
-        <Column>
-          <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
-            Order ID
-          </Text>
-          <Text className="mt-0 mr-4">{order.id}</Text>
-        </Column>
-        <Column>
-          <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
-            Purchased On
-          </Text>
-          <Text className="mt-0 mr-4">
-            {dateFormatter.format(order.createdAt)}
-          </Text>
-        </Column>
-        <Column>
-          <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
-            Price Paid
-          </Text>
-          <Text className="mt-0 mr-4">
-            {formatCurrency(order.pricePaidInCents / 100)}
-          </Text>
-        </Column>
-      </Row>
-    </Section>
+    <>
+      {/* Order info */}
+      <Section>
+        <Row>
+          <Column>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Order ID
+            </Text>
+            <Text className="mt-0 mr-4">{order.id}</Text>
+          </Column>
+          <Column>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Purchased On
+            </Text>
+            <Text className="mt-0 mr-4">
+              {dateFormatter.format(order.createdAt)}
+            </Text>
+          </Column>
+          <Column>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Price Paid
+            </Text>
+            <Text className="mt-0 mr-4">
+              {formatCurrency(order.pricePaidInCents / 100)}
+            </Text>
+          </Column>
+        </Row>
+      </Section>
+      {/* Product info */}
+      <Section className="border border-solid border-gray-600 rounded-lg p-4 md:p-6 my-4">
+        <Img
+          width="100%"
+          alt={product.name}
+          src={`${process.env.NEXT_PUBLIC_SERVER_URL}${product.imagePath}`}
+        />
+        <Row className="mt-6">
+          <Column className="align-bottom">
+            <Text className="text-lg font-bold m-0 mr-4">{product.name}</Text>
+          </Column>
+          <Column align="right">
+            <Button
+              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/products/download/${downloadVerificationId}`}
+              className="bg-black text-white px-6 py-4 rounded text-lg"
+            >
+              Download
+            </Button>
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <Text className="text-gray-600 mb-0">{product.description}</Text>
+          </Column>
+        </Row>
+      </Section>
+    </>
   );
 }
