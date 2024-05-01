507942ff60c6593f8a6ceaea2074b0ac1e01f300
Nikolai <niklonberg@gmail.com>
2024-05-01 10:02:47 +0200

    setup basic PurchaseReceiptEmail component

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -1,5 +1,27 @@
-import { Html } from "@react-email/components";
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./_components/OrderInformation";
 
-export default function PurchaseReceiptEmail() {
-  return <Html></Html>;
+export default function PurchaseReceiptEmail({ product }: { name: string }) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation></OrderInformation>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 }
